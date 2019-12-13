101dd09eaf69be643ce648c1e3203d81b5df32e8
luncheon <takeuck+github@gmail.com>
2019-12-13 14:24:20 +0900

    feat: 星座の名前を追加

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+import { Vector3 } from 'three'
+import _constellations from '../data/constellations.json'
+import _stars from '../data/stars.json'
+
+export const stars = _stars.map(([hip, name, rightAscension, declination, magnitude]) => {
+  const a = (rightAscension / 86400) * 2 * Math.PI
+  const b = (declination / 3600 / 180) * Math.PI
+  const normal = new Vector3(Math.cos(a) * Math.cos(b), Math.sin(a) * Math.cos(b), Math.sin(b))
+  return {
+    hip,
+    name,
+    magnitude,
+    get normal() {
+      return normal.clone()
+    },
+  }
+})
+
+const hipStarMap = Object.fromEntries(stars.map(star => [star.hip, star]))
+
+export const constellations = _constellations.map(([name, lines]) => ({
+  name,
+  lines: lines.map(([hip1, hip2]) => [hipStarMap[hip1], hipStarMap[hip2]] as const).filter(([star1, star2]) => star1 && star2),
+}))
--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,92 @@
-import * as THREE from 'three'
+import {
+  Color,
+  Geometry,
+  LinearFilter,
+  LineBasicMaterial,
+  LineSegments,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Points,
+  PointsMaterial,
+  Scene,
+  Texture,
+  Vector3,
+  VertexColors,
+  WebGLRenderer,
+} from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import _stars from '../data/stars.json'
-import constellations from '../data/constellations.json'
+import { constellations, stars } from './data'
 
-const stars = _stars.map(([hip, name, rightAscension, declination, magnitude]) => {
-  const a = (rightAscension / 86400) * 2 * Math.PI
-  const b = (declination / 3600 / 180) * Math.PI
-  const normal = new THREE.Vector3(Math.cos(a) * Math.cos(b), Math.sin(a) * Math.cos(b), Math.sin(b))
-  return {
-    hip,
-    name,
-    magnitude,
-    get normal() {
-      return normal.clone()
-    },
-  }
-})
+const scene = new Scene()
 
-const scene = new THREE.Scene()
+// stars
 {
-  const geometry = new THREE.Geometry()
+  const geometry = new Geometry()
   geometry.vertices = stars.map(star => star.normal.multiplyScalar(1.2 ** star.magnitude))
-  geometry.colors = stars.map(star => new THREE.Color().setScalar(0.8 ** star.magnitude))
-  scene.add(new THREE.Points(geometry, new THREE.PointsMaterial({ size: 0.01, vertexColors: THREE.VertexColors })))
+  geometry.colors = stars.map(star => new Color().setScalar(0.8 ** star.magnitude))
+  scene.add(new Points(geometry, new PointsMaterial({ size: 0.01, vertexColors: VertexColors })))
 }
+
+// constellation lines
 {
-  const hipStarMap = Object.fromEntries(stars.map(star => [star.hip, star]))
-  const createLineVertex = (star1: typeof stars[0], star2: typeof stars[0]) =>
-    star1.normal.multiplyScalar(100).add(star2.normal.sub(star1.normal).normalize())
-  const geometry = new THREE.Geometry()
-  for (const [, lines] of constellations) {
-    for (const [hip1, hip2] of lines) {
-      const star1 = hipStarMap[hip1]
-      const star2 = hipStarMap[hip2]
-      if (star1 && star2) {
-        geometry.vertices.push(createLineVertex(star1, star2), createLineVertex(star2, star1))
-      }
+  const geometry = new Geometry()
+  for (const { lines } of constellations) {
+    for (const [star1, star2] of lines) {
+      geometry.vertices.push(
+        star1.normal.multiplyScalar(100).add(star2.normal.sub(star1.normal).normalize()),
+        star2.normal.multiplyScalar(100).add(star1.normal.sub(star2.normal).normalize()),
+      )
     }
   }
-  scene.add(new THREE.LineSegments(geometry, new THREE.LineBasicMaterial({ color: 0x888888 })))
+  scene.add(new LineSegments(geometry, new LineBasicMaterial({ color: 0x5555aa })))
+}
+
+// constellation names
+{
+  const createTextCanvas = (text: string) => {
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')! // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    context.font = '100 100px serif'
+    canvas.width = Math.ceil(context.measureText(text).width * 1.1)
+    canvas.height = 110
+    context.font = '100 100px serif'
+    context.textAlign = 'center'
+    context.textBaseline = 'middle'
+    context.fillStyle = '#668'
+    context.fillText(text, canvas.width / 2, canvas.height / 2)
+    return canvas
+  }
+  const createCanvasMesh = (canvas: HTMLCanvasElement, position: Vector3) => {
+    const texture = new Texture(canvas)
+    texture.minFilter = LinearFilter
+    texture.needsUpdate = true
+    const mesh = new Mesh(new PlaneGeometry(canvas.width, canvas.height), new MeshBasicMaterial({ map: texture }))
+    mesh.position.copy(position)
+    mesh.lookAt(position.clone().normalize())
+    return mesh
+  }
+  for (const { name, lines } of constellations) {
+    const stars = lines.flat().filter((star, i, stars) => stars.indexOf(star) === i)
+    const normal = stars.reduce((normal, star) => normal.add(star.normal), new Vector3()).normalize()
+    scene.add(createCanvasMesh(createTextCanvas(name), normal.multiplyScalar(3000)))
+  }
 }
 
 const width = window.innerWidth
 const height = window.innerHeight
-const camera = new THREE.PerspectiveCamera(50, width / height, 0.1, 1000)
-camera.position.z = 1e-6
+const camera = new PerspectiveCamera(50, width / height, 0.1, 4000)
+camera.position.set(0, 0, -1e-6)
+camera.lookAt(0, 0, 1)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new WebGLRenderer()
 renderer.setSize(width, height)
 document.body.insertBefore(renderer.domElement, document.body.firstChild)
 renderer.render(scene, camera)
 
 const controls = new OrbitControls(camera, document.body)
-controls.minDistance = controls.maxDistance = camera.position.z
+controls.minDistance = controls.maxDistance = camera.position.distanceTo(new Vector3())
 controls.addEventListener('change', () => renderer.render(scene, camera))
 
 addEventListener('resize', () => {
