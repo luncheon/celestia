d3412e0937c0e51b9e30cec67aed3f088586b976
luncheon <takeuck+github@gmail.com>
2019-12-19 19:42:11 +0900

    feat: デバイスの向きとカメラのオイラー角を表示するよう変更、デフォルトのデバイスの向き調整

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,8 @@
-import { Object3D, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
+import { Object3D, PerspectiveCamera, Scene, WebGLRenderer, Euler } from 'three'
 import { DeviceOrientationControls } from 'three/examples/jsm/controls/DeviceOrientationControls'
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
+
+const getElementById = (id: string) => document.getElementById(id)! // eslint-disable-line @typescript-eslint/no-non-null-assertion
 
 const supportsDeviceOrientationEvent = new Promise(resolve => {
   let deviceOrientationEventCount = 0
@@ -39,21 +41,48 @@ export const start = async (objects: Object3D[]) => {
 
     const deviceOrientationControls = new DeviceOrientationControls(camera)
 
-    let activeControls: { update(): void } = trackballControls
-    const animate = () => {
-      activeControls.update()
-      renderer.render(scene, camera)
-      requestAnimationFrame(animate)
+    let update = (): unknown => {
+      trackballControls.update()
+      return 1
     }
-
     await Promise.race([
       supportsDeviceOrientationEvent.then(() => {
         trackballControls.dispose()
-        activeControls = deviceOrientationControls
+        getElementById('device-orientations').style.display = ''
+        const alphaElement = getElementById('device-orientation-alpha')
+        const betaElement = getElementById('device-orientation-beta')
+        const gammaElement = getElementById('device-orientation-gamma')
+        update = () => {
+          const deviceOrientation = deviceOrientationControls.deviceOrientation
+          if (deviceOrientationControls.deviceOrientation instanceof DeviceOrientationEvent) {
+            const alpha = deviceOrientation.alpha || 0
+            const beta = deviceOrientation.beta || 0
+            const gamma = deviceOrientation.gamma || 0
+            alphaElement.textContent = (Math.round(alpha) as string | number) as string
+            betaElement.textContent = (Math.round(beta) as string | number) as string
+            gammaElement.textContent = (Math.round(gamma) as string | number) as string
+            deviceOrientationControls.deviceOrientation = {
+              alpha: alpha + 180,
+              beta: beta - 35 /* latitude */,
+              gamma: gamma,
+            }
+            deviceOrientationControls.update()
+            return 1
+          }
+        }
       }),
       timeoutsDeviceOrientationEvent,
     ])
 
+    const cameraEulerIndicator = getElementById('camera-euler')
+    const animate = () => {
+      if (update()) {
+        const euler = new Euler().setFromRotationMatrix(camera.matrix, 'YXZ')
+        cameraEulerIndicator.textContent = `Camera: (${euler.x.toFixed(2)}, ${euler.y.toFixed(2)}, ${euler.z.toFixed(2)}) [${euler.order}]`
+        renderer.render(scene, camera)
+      }
+      requestAnimationFrame(animate)
+    }
     animate()
   }
 }
